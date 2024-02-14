0c379f7c88aa8ad2ad045025d9de876c65b07f59
Iván <133716241+imprvhub@users.noreply.github.com>
2024-02-14 02:51:31 -0300

    feat: ocr pdf impl

--- api/index.py
+++ api/index.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, redirect
 import pytesseract
 from PIL import Image
 import os
+import PyPDF2
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -28,11 +29,26 @@ def upload():
     for file in files:
         if file.filename == '':
             continue
-        image = Image.open(file)
-        text = ocr_with_tesseract(image)
-        detected_texts.append({'paragraphs': [text]})
+        if file.filename.lower().endswith(('.png', '.jpg', '.jpeg')):
+            image = Image.open(file)
+            text = ocr_with_tesseract(image)
+            detected_texts.append({'paragraphs': [text]})
+        elif file.filename.lower().endswith('.pdf'):
+            text = ocr_pdf(file)
+            detected_texts.append({'paragraphs': [text]})
+        else:
+            continue
 
     return render_template('result.html', detected_texts=detected_texts)
+
+def ocr_pdf(file):
+    pdf_text = ''
+    reader = PyPDF2.PdfReader(file)
+    num_pages = len(reader.pages)
+    for page_num in range(num_pages):
+        page = reader.pages[page_num]
+        pdf_text += page.extract_text()
+    return pdf_text
 
 @app.route('/user_agreements')
 def user_agreements():
